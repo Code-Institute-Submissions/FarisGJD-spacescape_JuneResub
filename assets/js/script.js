2237b1cfa68b7f52ad69fb529dd47ecb6b969bff
Faris Dhoot <farisgjd@outlook.com>
2021-08-19 01:58:27 +0000

    Create boilerplate code for Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+// Waits For DOM To Finnish Loading Before Starting The  Game
+document.addEventListener ("DOMContentLoaded")
+function gameStart() {
+
+}
+
+function openModels() {
+
+}
+
+function activateHardMode() {
+
+}
+
+function hardMode () {
+
+}
+
+function toggleSound() {
+
+}
+
+function gameSounds() {
+
+} 
+
+function playerControls() {
+
+}
+
+function obstacleDetection () {
+
+}
+
+function scoreCounter () {
+
+}
+
+function gameOver() {
+
+}
+
+
+
