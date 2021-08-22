2b0a58b8ce8766737fbc2be4b050388aeb6cee2b
Faris Dhoot <farisgjd@outlook.com>
2021-08-22 20:37:13 +0000

    Fixing touch event listner for mobile phones and add game.html document

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,39 +4,47 @@ let scoreCounter = document.getElementById("score-counter");
 let spaceObstacle = document.getElementsByClassName("space-obstacle"); 
 
 function jump () {
-    sprite.classList.add('animate-sprite'); 
+    sprite.classList.add("animate-sprite"); 
     setTimeout(() => {
-        sprite.classList.remove('animate-sprite'); 
+        sprite.classList.remove("animate-sprite"); 
     }, 800); 
 }
 
-document.addEventListener('click', () => {
-    if(!sprite.classList.contains('animate-sprite')) {
+document.addEventListener("keypress", () => {
+    if(!sprite.classList.contains("animate-sprite")) {
         jump(); 
 
     }
 });    
 
-//Game Over Detection 
 
-let gameOver = setInterval(function () {
+document.addEventListener("touchstart", () => {
+    if(!sprite.classList.contains("animate-sprite")) {
+        jump(); 
 
-    let spriteUpper = window.getComputedStyle(sprite).getPropertyValue("Upper"); 
+    }
+});    
+
+// //Game Over Detection 
+
+// let gameOver = setInterval(function () {
+
+//     let spriteUpper = window.getComputedStyle(sprite).getPropertyValue("Upper"); 
     
-}, 10); 
+// }, 10); 
 
 
-// Player Settings - Modal 
+// Player Settings - Modals 
 
-const modalBtn = document.querySelector('.open-modal'); 
-const modalBg = document.querySelector('.outer-modal');
-const modalClose = document.querySelector('.close-modal'); 
+const modalBtnRules = document.querySelector(".open-rules-modal"); 
+const modalBg = document.querySelector(".outer-modal");
+const modalClose = document.querySelector(".close-modal"); 
 
-modalBtn.addEventListener('click', function() {
-    modalBg.classList.add('modal-toggle')
+modalBtnRules.addEventListener("click", function() {
+    modalBg.classList.add("modal-toggle")
 }); 
 
-modalClose.addEventListener('click', function () {
-    modalBg.classList.remove('modal-toggle'); 
+modalClose.addEventListener("click", function () {
+    modalBg.classList.remove("modal-toggle")
 }); 
 
