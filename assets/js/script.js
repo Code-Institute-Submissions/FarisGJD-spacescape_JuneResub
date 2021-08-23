6f35c767caadad0a3a00165cd6d2a9b83fced2b3
Faris Dhoot <farisgjd@outlook.com>
2021-08-23 18:04:09 +0000

    Create starter code for  random obstacle generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ let sprite = document.getElementById("astronaut-sprite");
 let scoreCounter = document.getElementById("score-counter");
 let spaceObstacle = document.querySelector(".space-obstacle");
 
+
 function  locomotionJump () {
     sprite.classList.add("animate-sprite"); 
     setTimeout(() => {
@@ -29,15 +30,29 @@ setInterval (() => {
     .getPropertyValue("top")); 
     const obstacleLeft = parseInt(window.getComputedStyle(spaceObstacle)
     .getPropertyValue("left")); 
-    console.log(obstacleLeft)
-
 
     if (obstacleLeft < 50 && obstacleLeft > 0 && spriteTop > 250) {
-        alert("Game Over! You Got A Score Of: " + scoreCounter.innerText);
-        location.reload();
+        // alert("Game Over! You Got A Score Of: " + scoreCounter.innerText);
+        // location.reload();
     }
 }, 50); 
 
+// Random Obstacle Generator 
+const spaceObstacleMeteorite = document.getElementsByClassName("meteorite");
+const spaceObstacleAsteroid = document.getElementsByClassName("asteroid");  
+const spaceObstacleFireBall = document.getElementsByClassName("fire-ball");  
+const spaceObstacles = [spaceObstacleMeteorite + spaceObstacleAsteroid + spaceObstacleFireBall]; 
+
+console.log(spaceObstacles);
+
+function randomObstacleGenerator () {
+    Math.random(spaceObstacle);  
+};
+
+
+// let spaceObstacleRandom = Math.floor(Math.random() * 10);
+
+// console.log(spaceObstacleRandom); 
 
 
 
